a468c1fdd6d13abb59914f8cbe1575961d00c41e
Akshay <akshay@nighthack.in>
2020-10-29 14:05:19 +0530

    Camera permission android

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Platform } from 'react-native';
 
 import { WebView } from 'react-native-webview';
+import { PermissionsAndroid } from 'react-native';
 
 export default App = () => {
+  useEffect(() => {
+    const checkPerm = async () => {
+      try {
+        if (Platform.OS === 'android') {
+          const permission = PermissionsAndroid.PERMISSIONS.CAMERA;
+          const status = await PermissionsAndroid.request(permission);
+          console.log('Status: ', status);
+        }
+      } catch (err) {
+        console.log('error', err);
+      }
+    };
+    checkPerm();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://telehealthtest.tk/user/login' }} />
+      <WebView
+        source={{ uri: 'https://www.onlinemictest.com/webcam-test/' }}
+        mediaPlaybackRequiresUserAction={false}
+        javaScriptEnabled={true}
+      />
     </View>
   );
 };
